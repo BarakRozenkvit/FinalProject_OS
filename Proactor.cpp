2341fc40a95ba0da024098324a9d5b2138d558e3
BarakRozenkvit <barak10101998@gmail.com>
2024-12-10 21:12:04 +0200

    isolate proactor lib and fix logic

--- Proactor.cpp
+++ Proactor.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-std::vector<std::pair<pthread_t,void*>> proactors; // Definition
-pthread_cond_t condProactor = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mutexProactor = PTHREAD_MUTEX_INITIALIZER;
-
 // Wrapper function.
 void* proactorWrapper(void* arg) {
     proactorArgs* data = static_cast<proactorArgs*>(arg);
