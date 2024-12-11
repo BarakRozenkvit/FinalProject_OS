d562f0d11865c7ddc6b2132580d5d617aae6a132
BarakRozenkvit <barak10101998@gmail.com>
2024-12-11 11:58:08 +0200

    add clear terminal

--- ClientHandler.cpp
+++ ClientHandler.cpp
@@ -4,49 +4,43 @@ std::vector<std::pair<pthread_t,void*>> handlers; // Definition
 pthread_cond_t condHandler = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutexHandler = PTHREAD_MUTEX_INITIALIZER;
 
+string CLEAR_TERMINAL = "\033[2J\033[H";
+
 int ClientHandler::handleGraph(int fd) {
-    ClientHandler::outputHandler("\033[2J\033[H",fd);
-    ClientHandler::outputHandler("Requesting Permission to Graph...\n",fd);
+    ClientHandler::outputHandler(CLEAR_TERMINAL + "Requesting Permission to Graph...\n",fd);
     MainGraph* graph = MainGraph::getInstance();
     MainGraph::lockInstance();
-    ClientHandler::outputHandler("Permission Granted!\n",fd);
-    string cmd = ClientHandler::inputHandler("Write a Command:"
+    string cmd = ClientHandler::inputHandler(CLEAR_TERMINAL + "Write a Command:"
                                              "\n- Newgraph [to create new graph]"
                                              "\n- MST (calculate mst to graph)"
-                                             "\n- "
-                                             "Newedge (add edge)"
+                                             "\n- Newedge (add edge)"
                                              "\n- Removeedge (remove edge)"
+                                             "\n- Exit"
                                              "\nYour Input: ",fd);
     int exit = 0;
     if (cmd == "Newgraph") {
-        ClientHandler::outputHandler("\033[2J\033[H",fd);
-        string number = ClientHandler::inputHandler("Choose graph size\nYour Input: ", fd);
+        string number = ClientHandler::inputHandler(CLEAR_TERMINAL + "Choose graph size\nYour Input: ", fd);
         try {
             int size = stoi(number);
             graph->newGraph(size);
         }
         catch (const std::invalid_argument& e) {
-            ClientHandler::outputHandler("\033[2J\033[H",fd);
-            ClientHandler::outputHandler("Number not provided!",fd);
+            ClientHandler::outputHandler(CLEAR_TERMINAL + "Number not provided!",fd);
             sleep(1);
         }
     }
     else if (cmd == "MST") {
-        ClientHandler::outputHandler("\033[2J\033[H",fd);
-        string algo = ClientHandler::inputHandler("Choose MST algorithm\n- Prim\n- Kruskal\nYour Input: ", fd);
+        string algo = ClientHandler::inputHandler(CLEAR_TERMINAL + "Choose MST algorithm\n- Prim\n- Kruskal\nYour Input: ", fd);
         try {
             MSTAlgo::FactoryAlgo::applyAlgo(graph, algo);
         }
         catch (const invalid_argument& e){
-            ClientHandler::outputHandler("\033[2J\033[H",fd);
-            ClientHandler::outputHandler(e.what(),fd);
+            ClientHandler::outputHandler(CLEAR_TERMINAL + e.what(),fd);
             sleep(1);
         }
     }
     else if (cmd == "Newedge") {
-        ClientHandler::outputHandler("\033[2J\033[H",fd);
-        ClientHandler::outputHandler("Add Edge:\n[format: v u w]\nYour Input:",fd);
-        string input = ClientHandler::inputHandler("Add Edge:\n[format: v u w]\nYour Input: ",fd);
+        string input = ClientHandler::inputHandler(CLEAR_TERMINAL + "Add Edge:\n[format: v u w]\nYour Input: ",fd);
         stringstream inputStream(input);
         string vstr,ustr,wstr;
         inputStream >> vstr;
@@ -59,14 +53,12 @@ int ClientHandler::handleGraph(int fd) {
             graph->addEdge(v,u,w);
         }
         catch (const invalid_argument& e) {
-            ClientHandler::outputHandler("\033[2J\033[H",fd);
-            ClientHandler::outputHandler(e.what(),fd);
+            ClientHandler::outputHandler(CLEAR_TERMINAL + e.what(),fd);
             sleep(1);
         }
     }
     else if (cmd == "Removeedge") {
-        ClientHandler::outputHandler("\033[2J\033[H",fd);
-        string input = ClientHandler::inputHandler("Remove Edge:\n[format: v u]\nYour Input: ",fd);
+        string input = ClientHandler::inputHandler(CLEAR_TERMINAL + "Remove Edge:\n[format: v u]\nYour Input: ",fd);
         stringstream inputStream(input);
         string vstr,ustr;
         inputStream >> vstr;
@@ -77,8 +69,7 @@ int ClientHandler::handleGraph(int fd) {
             graph->removeEdge(v,u);
         }
         catch (const invalid_argument& e) {
-            ClientHandler::outputHandler("\033[2J\033[H",fd);
-            ClientHandler::outputHandler(e.what(),fd);
+            ClientHandler::outputHandler(CLEAR_TERMINAL + e.what(),fd);
             sleep(1);
         }
     }
@@ -86,10 +77,9 @@ int ClientHandler::handleGraph(int fd) {
         exit = 1;
     }
     else {
-        ClientHandler::outputHandler("Unknown command!",fd);
+        ClientHandler::outputHandler(CLEAR_TERMINAL + "Unknown command!",fd);
         sleep(1);
     }
-
     MainGraph::unlockInstance();
     return !exit;
 }
@@ -132,6 +122,9 @@ string ClientHandler::inputHandler(string message,int fd) {
 
     buffer[receiveBytes - 1] = '\0';
     string input(buffer);
+    if (input == "Exit"){
+        close(fd);
+    }
 
     return input;
 }
@@ -153,11 +146,14 @@ void ClientHandler::outputHandler(string message,int fd) {
 void* ClientHandler::monitorHandlers(int){
     while(1){
         pthread_cond_wait(&condHandler,&mutexHandler);
-        for (auto id: handlers){
-            proactorArgs* data = static_cast<proactorArgs*>(id.second);
+        for( auto it = handlers.begin(); it != handlers.end();){
+            proactorArgs* data = static_cast<proactorArgs*>((*it).second);
             if(data->pause){
-                cout << "Stopping proactor fd: " << data->sockfd << endl;
-                stopProactor(id.first,data);
+                stopProactor((*it).first,data);
+                handlers.erase(it);
+            }
+            else{
+                ++it;
             }
         }
     }
