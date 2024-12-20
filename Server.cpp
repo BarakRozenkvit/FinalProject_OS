6f80df350c05fea0c52797d1acff9c2e043136b2
BarakRozenkvit <barak10101998@gmail.com>
2024-12-20 14:36:46 +0200

    fix

--- Server.cpp
+++ Server.cpp
@@ -73,32 +73,6 @@ void listThreads(pid_t pid) {
     }
 }
 
-void listThreads() {
-    #include <mach/mach.h>
-
-    // Get the current task
-    mach_port_t task;
-    task_for_pid(mach_task_self(), getpid(), &task);
-
-    // Get thread list
-    thread_act_array_t threadList;
-    mach_msg_type_number_t threadCount;
-
-    kern_return_t kr = task_threads(task, &threadList, &threadCount);
-    if (kr != KERN_SUCCESS) {
-        std::cerr << "Error: Unable to get thread list. Error code: " << kr << std::endl;
-        return;
-    }
-
-    std::cout << "Total threads: " << threadCount << std::endl;
-    for (mach_msg_type_number_t i = 0; i < threadCount; ++i) {
-        std::cout << "Thread ID: " << threadList[i] << std::endl;
-    }
-
-    // Deallocate thread list
-    vm_deallocate(mach_task_self(), reinterpret_cast<vm_address_t>(threadList), threadCount * sizeof(thread_act_t));
-}
-
 void signalHandler(int signal){
     if (signal == SIGINT){
         std::cout << "shutting down gracefully..." << std::endl;
