3f5afb5717730e41c9acc9832261dddc5ed1b280
BarakRozenkvit <barak10101998@gmail.com>
2024-12-20 15:02:41 +0200

    fix procator

--- Proactor.hpp
+++ Proactor.hpp
@@ -19,9 +19,6 @@ using namespace std;
 // Function type for clients threads
 typedef void * (* proactorClient) (int sockfd);
 
-// Function type for active objects threads
-typedef void* (* proactorPipeline) (void* stage);
-
 // Structs to hold the original function and its argument
 typedef struct {
     proactorClient func;
@@ -29,22 +26,11 @@ typedef struct {
     bool pause;
 } proactorArgsClient;
 
-typedef struct {
-    proactorPipeline func;
-    void* stage;
-} proactorArgsPipeline;
-
 // Wrapper functions for proactor
 void* proactorWrapperClient(void* arg);
-
-void* proactorWrapperPipeline(void* arg);
 
 // starts new proactor and returns proactor thread id and ThreadArgs.
 pair<pthread_t,void*> startProactorClient(int sockfd, proactorClient threadFunc);
 
-pair<pthread_t,void*> startProactorPipeline(void* stage, proactorPipeline threadFunc);
-
 // stops proactor by threadid and free Thread Args
 int stopProactorClient(pthread_t tid,proactorArgsClient* args);
-
-int stopProactorPipeline(pthread_t tid,proactorArgsPipeline* args);
